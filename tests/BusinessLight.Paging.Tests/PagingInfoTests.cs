6023a9c385f3111afafb303b04bb41580b6f9c2b
martinobordin <info@martinobordin.it>
2015-11-18 13:10:34 +0100

    More tests

--- tests/BusinessLight.Paging.Tests/PagingInfoTests.cs
+++ tests/BusinessLight.Paging.Tests/PagingInfoTests.cs
@@ -22,5 +22,21 @@ namespace BusinessLight.Paging.Tests
             pagingInfo.IsFirstPage.Should().Be.True();
             pagingInfo.IsLastPage.Should().Be.False();
         }
+
+        [TestMethod]
+        public void GivenAListPagingInfoShouldBeCorrects1()
+        {
+            var pagingInfo = new PagingInfo(0, 10, 10);
+            pagingInfo.TotalItemCount.Should().Be.EqualTo(10);
+            pagingInfo.PageCount.Should().Be.EqualTo(1);
+            pagingInfo.FirstItemOnPage.Should().Be.EqualTo(0);
+            pagingInfo.LastItemOnPage.Should().Be.EqualTo(9);
+            pagingInfo.HasPreviousPage.Should().Be.False();
+            pagingInfo.HasNextPage.Should().Be.False();
+            pagingInfo.PageNumber.Should().Be.EqualTo(0);
+            pagingInfo.PageSize.Should().Be.EqualTo(10);
+            pagingInfo.IsFirstPage.Should().Be.True();
+            pagingInfo.IsLastPage.Should().Be.True();
+        }
     }
 }
