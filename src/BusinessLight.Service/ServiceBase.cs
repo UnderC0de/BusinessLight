2a19ab527d3eb5bba6c0cdfeaacd47b08a37e5aa
martinobordin <info@martinobordin.it>
2015-11-18 16:53:48 +0100

    BusinessLight.Service

---
+++ src/BusinessLight.Service/ServiceBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BusinessLight.Data;
+
+namespace BusinessLight.Service
+{
+    public abstract class ServiceBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        protected ServiceBase(IUnitOfWork unitOfWork)
+        {
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unitOfWork");
+            }
+
+            _unitOfWork = unitOfWork;
+        }
+
+        protected IUnitOfWork GetUnitOfWork()
+        {
+            return _unitOfWork;
+        }
+    }
+}
