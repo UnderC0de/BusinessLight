126174e85bca53406f14cde3517d9843d0d77892
Martino Bordin <martino.bordin@addvalue.it>
2015-11-18 12:47:33 +0100

    Added BusinessLight.PhoneBook.Data

---
+++ samples/BusinessLight.PhoneBook.Data/PhoneBookDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLight.PhoneBook.Domain;
+
+namespace BusinessLight.PhoneBook.Data
+{
+    using System.Data.Entity;
+
+    public class PhoneBookDbContext : DbContext
+    {
+        public PhoneBookDbContext()
+            : base("name=PhoneBookDbContext")
+        {
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; }
+    }
+}
