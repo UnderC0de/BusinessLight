53b49c740c20e5963f9e6888b7b41df836e3c9d6
martinobordin <info@martinobordin.it>
2015-11-24 11:50:32 +0100

    Added PhoneBookDbContextSeedInitializer

---
+++ samples/BusinessLight.PhoneBook.Data/PhoneBookDbContextSeedInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+using BusinessLight.PhoneBook.Domain;
+
+namespace BusinessLight.PhoneBook.Data
+{
+    public class PhoneBookDbContextSeedInitializer : DropCreateDatabaseIfModelChanges<PhoneBookDbContext>
+    {
+        protected override void Seed(PhoneBookDbContext context)
+        {
+            for (var i = 0; i < 150; i++)
+            {
+                var contact = new Contact
+                {
+                    FirstName = string.Format("FirstName{0}", i),
+                    LastName = string.Format("LastName{0}", i),
+                    BirthDate = DateTime.Now.AddDays(i),
+                    Note = string.Format("Note1{0}", i),
+                };
+                context.Set<Contact>().Add(contact);
+            }
+            context.SaveChanges();
+        }
+    }
+}
