9551aa5c3f763eb0c012d84d85b671ef2fb77eb5
martinobordin <info@martinobordin.it>
2015-11-24 11:26:56 +0100

    Added FluentValidator

---
+++ src/BusinessLight.Validation.Fluent/FluentValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using FluentValidation;
+
+namespace BusinessLight.Validation.Fluent
+{
+    public class FluentValidator<T> : AbstractValidator<T>, IValidator<T>
+    {
+        public ValidationResult GetValidationResult(T instance)
+        {
+            var validationIssues = Validate(instance)
+                .Errors
+                .Select(x => new ValidationIssue(x.ErrorMessage));
+
+            return new ValidationResult(validationIssues);
+        }
+    }
+}
