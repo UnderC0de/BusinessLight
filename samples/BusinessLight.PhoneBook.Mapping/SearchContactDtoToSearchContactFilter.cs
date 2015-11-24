604aecbd06abed3417305d480b4ec8b299f09d17
martinobordin <info@martinobordin.it>
2015-11-24 11:24:32 +0100

    Added SearchContactDtoToSearchContactFilter

---
+++ samples/BusinessLight.PhoneBook.Mapping/SearchContactDtoToSearchContactFilter.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BusinessLight.PhoneBook.Dto.Filters;
+using BusinessLight.PhoneBook.Service.Filters;
+
+namespace BusinessLight.PhoneBook.Mapping
+{
+    public class SearchContactDtoToSearchContactFilterProfile : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<SearchContactDto, SearchContactFilter>();
+        }
+    }
+}
