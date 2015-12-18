299a61b5ef732367145f7b75f5426f32ba4c9a44
Martino Bordin <info@martinobordin.it>
2015-12-18 12:21:00 +0100

    Added missing files

---
+++ samples/BusinessLight.PhoneBook.Validation/ContactValidator.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLight.PhoneBook.Domain;
+using BusinessLight.Validation.Fluent;
+using FluentValidation;
+
+namespace BusinessLight.PhoneBook.Validation
+{
+    public class ContactValidator : FluentValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(filter => filter.FirstName).NotEmpty();
+            RuleFor(filter => filter.LastName).NotEmpty();
+        }
+    }
+}
