5236f03abe2712b1ab571c20ab1877365bf57dbe
Martino Bordin <info@martinobordin.it>
2016-01-11 17:22:02 +0100

    Added AngularJS custom filters

---
+++ samples/BusinessLight.PhoneBook.Angular/Scripts/filters/businesslight-filter.js
@@ -0,0 +1,21 @@
+﻿angular.module("customFilters", [])
+.filter("unique", function () {
+    return function (data, propertyName) {
+        // Filters 'data' items, returnin a list of unique values for 'propertyName'
+        if (angular.isArray(data) && angular.isString(propertyName)) {
+            var results = [];
+            var keys = {};
+            for (var i = 0; i < data.length; i++) {
+                var val = data[i][propertyName];
+                if (angular.isUndefined(keys[val])) {
+                    keys[val] = true;
+                    results.push(val);
+                }
+            }
+            return results;
+        } else {
+            return data;
+        }
+    }
+});
+
