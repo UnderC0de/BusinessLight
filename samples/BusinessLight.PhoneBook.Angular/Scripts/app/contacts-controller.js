7bde353323549b581faf5d7339c5deeeaed2e374
Martino Bordin <info@martinobordin.it>
2016-01-14 17:35:53 +0100

    AngularJs improvements

--- samples/BusinessLight.PhoneBook.Angular/Scripts/app/contacts-controller.js
+++ samples/BusinessLight.PhoneBook.Angular/Scripts/app/contacts-controller.js
@@ -1,19 +1,23 @@
-﻿var phoneBookApp = angular.module("PhoneBookApp", ["ngRoute", "ui.bootstrap"])
-    .constant("dataUrl", "http://localhost:51121/api/contact/search")
+﻿var phoneBookApp = angular.module("phoneBookApp", ["ngRoute", "ui.bootstrap"])
+    .constant("searchUrl", "http://localhost:51121/api/contact/search")
+    .constant("getByIdUrl", "http://localhost:51121/api/contact/get")
     .config(function ($routeProvider) {
         $routeProvider.when("/list", {
-            templateUrl: "/scripts/views/contacts/contactList.html"
+            templateUrl: "/scripts/views/contacts/contactList.html",
+            controller: "contactsController"
         });
 
-        $routeProvider.when("/edit", {
-            templateUrl: "/scripts/views/contacts/contactEdit.html"
+        $routeProvider.when("/edit/:id", {
+            templateUrl: "/scripts/views/contacts/contactEdit.html",
+            controller: "contactController"
         });
 
         $routeProvider.otherwise({
-            templateUrl: "/scripts/views/contacts/contactList.html"
+            templateUrl: "/scripts/views/contacts/contactList.html",
+            controller: "contactsController"
         });
     })
-    .controller("ContactsController", function ($scope, $http, dataUrl) {
+    .controller("contactsController", function ($scope, $http, searchUrl) {
         $scope.filter = {
             firstName: "",
             lastName: "",
@@ -30,21 +34,21 @@
         };
 
         $scope.search = function () {
-            //var data = {
-            //    FirstName: $scope.firstName,
-            //    LastName: $scope.lastName,
-            //    BirthDate: $scope.birthDate,
-            //    PageNumber: $scope.pageNumber - 1,
-            //    PageSize: $scope.pageSize,
-            //    SortField: $scope.sortField,
-            //    IsAscending: $scope.isAscending
-            //}
-            $http.post(dataUrl, $scope.filter)
-                .success(function (result, status) {
+            $http.post(searchUrl, $scope.filter)
+                .success(function (result) {
                     $scope.contacts = result;
                 })
                 .error(function (error) {
                     $scope.error = error;
                 });
         }
+    })
+    .controller("contactController", function ($scope, $http, $routeParams, getByIdUrl) {
+        var id = $routeParams.id;
+
+        $http.get(getByIdUrl, {
+            params: { id: id }
+        }).success(function (result) {
+            $scope.contact = result;
+        });
     });
