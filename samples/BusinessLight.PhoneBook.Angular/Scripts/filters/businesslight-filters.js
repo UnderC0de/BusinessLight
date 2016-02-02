5b86989a31a5ad991fd53054b881c6e85a3c2317
Martino Bordin <info@martinobordin.it>
2016-02-02 12:05:20 +0100

    labelCase filter

--- samples/BusinessLight.PhoneBook.Angular/Scripts/filters/businesslight-filters.js
+++ samples/BusinessLight.PhoneBook.Angular/Scripts/filters/businesslight-filters.js
@@ -17,5 +17,16 @@
             return data;
         }
     }
+})
+.filter("labelCase", function () {
+    return function (value, reverse) {
+        if (angular.isString(value)) {
+            var intermediate = reverse ? value.toUpperCase() : value.toLowerCase();
+            return (reverse ? intermediate[0].toLowerCase() :
+            intermediate[0].toUpperCase()) + intermediate.substr(1);
+        } else {
+            return value;
+        }
+    };
 });
 
