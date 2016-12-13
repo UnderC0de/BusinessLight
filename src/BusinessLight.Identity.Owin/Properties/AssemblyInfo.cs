18d017d44ce2e9de20541d59bc2627e5c3604b8d
Martino Bordin <info@martinobordin.it>
2016-12-13 15:13:41 +0100

    Assembly Info

--- src/BusinessLight.Identity.Owin/Properties/AssemblyInfo.cs
+++ src/BusinessLight.Identity.Owin/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BusinessLight.Identity.Owin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Asp.net identity owin authentication")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Martino Bordin")]
 [assembly: AssemblyProduct("BusinessLight.Identity.Owin")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
