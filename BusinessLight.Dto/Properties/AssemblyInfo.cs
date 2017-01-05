e56aa7584afd6bd69ab9160be53a6f65c505fdbd
Martino Bordin <info@martinobordin.it>
2017-01-05 12:09:44 +0100

    Code review

--- BusinessLight.Dto/Properties/AssemblyInfo.cs
+++ BusinessLight.Dto/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BusinessLight.Data.EntityFramework")]
-[assembly: AssemblyDescription("A library that implements an EntityFramework Code first repository and unit of work")]
+[assembly: AssemblyTitle("BusinessLight.Dto")]
+[assembly: AssemblyDescription("A library that helps to create data transfer object")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Martino Bordin")]
-[assembly: AssemblyProduct("BusinessLight.Data.EntityFramework")]
+[assembly: AssemblyProduct("BusinessLight.Dto")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aaa57e66-b61a-4371-9dcc-31089cdedd71")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
